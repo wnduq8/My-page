6dae2205bac6cb6b5e60008a0203c1801c0e64ca
wnduq8 <65497711+wnduq8@users.noreply.github.com>
2020-07-09 01:48:56 +0900

    add JS
    add JS

---
+++ main.js
@@ -0,0 +1,72 @@
+'use strict'
+
+// Navbar를 투명하게 만들고 navbar 높이만큼 내려가면 색이 진해짐
+const navbar = document.querySelector('#navbar');
+const navbarHeight = navbar.getBoundingClientRect().height;
+document.addEventListener('scroll', () => {
+    if (window.scrollY > navbarHeight) {
+        navbar.classList.add('navbar--dark');
+    } else {
+        navbar.classList.remove('navbar--dark');
+    }
+})
+
+//  Handle scrolling when tapping on the navbar menu
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click', (event) => {
+    const target = event.target;
+    const link = target.dataset.link;
+    if (link == null) {
+        return;
+    }
+    console.log(event.target.dataset.link);
+    const scrollTo = document.querySelector(link);
+    scrollTo.scrollIntoView({
+        behavior: 'smooth',
+        block: 'center'
+    });
+});
+
+// Handle click on "contact me" button on home
+const homeContactBtn = document.querySelector('.home__contact');
+homeContactBtn.addEventListener('click', () => {
+    const scrollTo = document.querySelector('#contact');
+    scrollTo.scrollIntoView({
+        behavior: 'smooth',
+        block: 'center'
+    });
+});
+
+// Make home slowly fade to transparent as the window scrolls down
+const home = document.querySelector('.Home__container');
+const homeHeight = home.getBoundingClientRect().height;
+document.addEventListener('scroll', () => {
+    home.style.opacity = 1 - window.scrollY / homeHeight;
+})
+
+// Show "arrow up" button when scrolling down
+const arrowUp = document.querySelector('.arrow_up');
+document.addEventListener('scroll', () => {
+    if (window.scrollY > homeHeight / 2) {
+        arrowUp.classList.add('visible');
+    } else {
+        arrowUp.classList.remove('visible');
+    }
+})
+
+// Handle click on the "arrow up" button
+arrowUp.addEventListener('click', () => {
+    scrollIntoView('#Home');
+})
+
+
+
+// scrollIntoView(selector); 호출해서 사용
+
+function scrollIntoView(selector) {
+    const scrollTo = document.querySelector(selector);
+    scrollTo.scrollIntoView({
+        behavior: 'smooth',
+        block: 'center'
+    });
+}
